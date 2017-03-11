d39ec6a8fe32348a06431b8615ccb8eb72e8a640
john-oberhauser <john.oberhauser@live.com>
2017-03-10 21:55:47 -0600

    capture

--- capture.py
+++ capture.py
@@ -1,9 +1,20 @@
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
 web_cam = cv2.VideoCapture(0)
+camera = PiCamera
+rawCapture = PiRGBArray(camera)
+
+# warm up camera
+time.sleep(0.1)
 
 while True:
-	ret, color = web_cam.read()
+	# ret, color = web_cam.read()
 	#cv2.imwrite('/messigray.png',color)
-	print color
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
+
+	print image
 	cv2.waitKey(500)
