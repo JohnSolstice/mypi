75c640d93e0d66b526a5ed55464c6d5586ff857c
john-oberhauser <john.oberhauser@live.com>
2017-03-10 22:03:04 -0600

    take pic

---
+++ take_pic.py
@@ -0,0 +1,17 @@
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+
+web_cam = cv2.VideoCapture(0)
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+# warm up camera
+time.sleep(0.1)
+
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+cv2.imwrite('/pic.png', image)
+cv2.waitKey(500)
