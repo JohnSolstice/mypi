a00e5cefff9b65600ba60a441c3bb2bc9f895750
john-oberhauser <john.oberhauser@live.com>
2017-03-10 22:19:05 -0600

    changes

--- capture.py
+++ capture.py
@@ -9,14 +9,14 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # warm up camera
-time.sleep(0.1)
+time.sleep(0.5)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
 
     #print image
     cv2.imwrite('pic.png', image)
-    #cv2.waitKey(500)
+    cv2.waitKey(500)
 
     rawCapture.truncate(0)
     break
