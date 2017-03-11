423215a72e6bc091eb7c05d8fcd817f6748a5fd7
john-oberhauser <john.oberhauser@live.com>
2017-03-10 23:02:42 -0600

    motion detection

--- capture.py
+++ capture.py
@@ -11,12 +11,36 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 # warm up camera
 time.sleep(0.5)
 
+previous_image = None
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
+    blurred = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(blurred, (21, 21), 0)
 
+    if previous_image is not None:
+        frameDelta = cv2.absdiff(previous_image, blurred)
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                     cv2.CHAIN_APPROX_SIMPLE)
+
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 250:
+                continue
+
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            print (x, y, w, h)
+
+    previous_image = blurred
     #print image
-    cv2.imwrite('pic.png', image)
+    # cv2.imwrite('pic.png', image)
     cv2.waitKey(500)
 
     rawCapture.truncate(0)
-    break
